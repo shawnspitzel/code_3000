1787a7ccad3ae1f3b8e4749f5e3c2a97fbfd56cd
ShawnSpitzel <ShawnSpitzel@gmail.com>
2026-03-30 19:29:15 -0400

    hw8

--- mod08_sustainability_calculations.py
+++ mod08_sustainability_calculations.py
@@ -8,9 +8,8 @@ def compute_energy_kwh(jobs_df, power_per_core_kw=POWER_PER_CORE_KW):
     Adds a column 'energy_kwh' to the jobs dataframe.
     """
     df = jobs_df.copy()
-    df["energy_kwh"] = ( 
-        # put energy calculation here
-
+    df["energy_kwh"] = (
+        df["cpu_cores"] * df["runtime_hours"] * power_per_core_kw
     )
     return df
 
